25bf34001c2dd18855eb5a402ac73b64879a715d
zhangsc <zhangsc@fingard.com>
2025-01-24 10:32:58 +0800

    feat: 项目NobodyIsPerfect-001-文本格式化-增加列表处理方式Doing

--- src/pages/Tools/TextFormatter/index.tsx
+++ src/pages/Tools/TextFormatter/index.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Checkbox, Input } from 'antd';
 import styles from './index.less';
 
+/**
+ * 格式化配置选项，由用户在页面选择
+ */
 interface CustomProperty {
   zhOrEn: boolean; // true-中文；false-英文；
   punctuationMark: boolean; // true-替换标点符号；false-不替换标点符号；
@@ -10,6 +13,17 @@ interface CustomProperty {
   withSpace: boolean; // true-英文、数字前后带空格；false-英文、数字前后不带空格；
   rewriteClipboard: boolean; // true-处理后复制到粘贴板；false-处理后不复制到粘贴板；
   isHandleClipboard: boolean; // true-从粘贴板复制内容到文本框中；false-不从粘贴板复制内容到文本框中；
+}
+
+/**
+ * 列表形内容配置选项，由用户在页面选择
+ */
+interface ListProperty {
+  removePrefixLength: number; // 要删除的前缀长度；
+  serialMark: string; // 编号，下拉可选：无 或 1. 2. 3. 或（1）（2）（3）或 一、二、三、或 a . b. c. 或 （a）（b）（c）或 * * * ；
+  endWith: string; // 要添加或替换的结束符，由用户输入，默认值为分号；
+  withBlankLine: boolean; // true-默认，添加空行；false-不添加空行；
+  lastWithPeriod: boolean; // 最后是否以句号（中文。，英文.）结束，默认为 true；
 }
 
 interface TextPair {
@@ -56,46 +70,127 @@ function handleChinese(customProp: CustomProperty, text: string) {
   if (customProp.compressSpace) {
     // 压缩空格
     //压缩空格
-    tempStr = tempStr.replace(/\s+/g, ' ');
+    tempStr = tempStr.replace(/ +/g, ' ');
     //去除中文之间的空格，由于存在“中 中 中 中”这样交叉空格情况，所以要调用两次才能清完
-    tempStr = tempStr.replace(/([\u4e00-\u9fa5])\s([\u4e00-\u9fa5])/g, '$1$2');
-    tempStr = tempStr.replace(/([\u4e00-\u9fa5])\s([\u4e00-\u9fa5])/g, '$1$2');
+    tempStr = tempStr.replace(/([\u4e00-\u9fa5]) ([\u4e00-\u9fa5])/g, '$1$2');
+    tempStr = tempStr.replace(/([\u4e00-\u9fa5]) ([\u4e00-\u9fa5])/g, '$1$2');
   }
   //去除标点符号和前后的空格
-  tempStr = tempStr.replace(/(\w)\s+([！，。？；（）‘’“”…【】])/g, '$1$2');
-  tempStr = tempStr.replace(/([！，。？；（）‘’“”…【】])\s+/g, '$1');
+  tempStr = tempStr.replace(/(\w) +([！，。？；（）‘’“”…、：【】])/g, '$1$2');
+  tempStr = tempStr.replace(/([！，。？；（）‘’“”…、：【】]) +/g, '$1');
   // 格式化数字
-  tempStr = tempStr.replace(/(\d)\s?[.。]\s?(\d)/g, '$1.$2');
-  tempStr = tempStr.replace(/(\d)\s?[,，]\s?(\d)/g, '$1, $2');
-  tempStr = tempStr.replace(/([$￥])\s?(\d)/g, '$1$2');
+  tempStr = tempStr.replace(/(\d) ?[.。] ?(\d)/g, '$1.$2');
+  tempStr = tempStr.replace(/(\d) ?[,，] ?(\d)/g, '$1, $2');
+  tempStr = tempStr.replace(/([$￥]) ?(\d)/g, '$1$2');
   // 格式化代码
-  tempStr = tempStr.replace(/\s?`\s?(.*?)\s?`\s?/g, ' `$1` ');
+  tempStr = tempStr.replace(/ ?` ?(.*?) ?` ?/g, ' `$1` ');
   // 英文之间的句点，形如 Node.js 还原
-  tempStr = tempStr.replace(/(\w)\s?[.。]\s?(\w)/g, '$1.$2');
+  tempStr = tempStr.replace(/(\w) ?[.。] ?(\w)/g, '$1.$2');
   return tempStr.trim();
+}
+
+const serialMark = [
+  { '0': '', '1': '1. ', '2': '（1）', '3': '一、', '4': 'a. ', '5': '（a）' },
+  { '0': '', '1': '2. ', '2': '（2）', '3': '二、', '4': 'b. ', '5': '（b）' },
+  { '0': '', '1': '3. ', '2': '（3）', '3': '三、', '4': 'c. ', '5': '（c）' },
+  { '0': '', '1': '4. ', '2': '（4）', '3': '四、', '4': 'd. ', '5': '（d）' },
+  { '0': '', '1': '5. ', '2': '（5）', '3': '五、', '4': 'e. ', '5': '（e）' },
+  { '0': '', '1': '6. ', '2': '（6）', '3': '六、', '4': 'f. ', '5': '（f）' },
+  { '0': '', '1': '7. ', '2': '（7）', '3': '七、', '4': 'g. ', '5': '（g）' },
+  { '0': '', '1': '8. ', '2': '（8）', '3': '八、', '4': 'h. ', '5': '（h）' },
+  { '0': '', '1': '9. ', '2': '（9）', '3': '九、', '4': 'i. ', '5': '（i）' },
+  { '0': '', '1': '10. ', '2': '（10）', '3': '十、', '4': 'j. ', '5': '（j）' },
+  { '0': '', '1': '11. ', '2': '（11）', '3': '十一、', '4': 'k. ', '5': '（k）' },
+  { '0': '', '1': '12. ', '2': '（12）', '3': '十二、', '4': 'l. ', '5': '（l）' },
+  { '0': '', '1': '13. ', '2': '（13）', '3': '十三、', '4': 'm. ', '5': '（m）' },
+  { '0': '', '1': '14. ', '2': '（14）', '3': '十四、', '4': 'n. ', '5': '（n）' },
+  { '0': '', '1': '15. ', '2': '（15）', '3': '十五、', '4': 'o. ', '5': '（o）' },
+  { '0': '', '1': '16. ', '2': '（16）', '3': '十六、', '4': 'p. ', '5': '（p）' },
+  { '0': '', '1': '17. ', '2': '（17）', '3': '十七、', '4': 'q. ', '5': '（q）' },
+  { '0': '', '1': '18. ', '2': '（18）', '3': '十八、', '4': 'r. ', '5': '（r）' },
+  { '0': '', '1': '19. ', '2': '（19）', '3': '十九、', '4': 's. ', '5': '（s）' },
+  { '0': '', '1': '20. ', '2': '（20）', '3': '二十、', '4': 't. ', '5': '（t）' },
+];
+
+function handleListChinese(listProp: ListProperty, text: string) {
+  let list = text.split(/[\r\n]+/g);
+  list = list.filter((item) => item && item !== '');
+  if (list.length === 0) {
+    return '';
+  }
+  // 最长只支持 20
+  const loopCount = Math.min(list.length, 20);
+  for (let i = 0; i < loopCount; i++) {
+    let str = list[i];
+    const length = str.length;
+    if (listProp.removePrefixLength > 0) {
+      // 删除前缀
+      str = str.substring(listProp.removePrefixLength, length);
+    }
+    str = str.trim();
+    // 添加编号
+    str = serialMark[i][listProp.serialMark] + str;
+    str = str.replace(/[,.，。；、]\w?$/, '');
+    str = str.trim();
+    if (i === list.length - 1) {
+      if (listProp.lastWithPeriod) {
+        // 最后一条换成句号结尾
+        str += '。';
+      }
+    } else {
+      // 添加指定末尾标点
+      str = str + listProp.endWith;
+      if (listProp.withBlankLine) {
+        // 加上换行
+        str += '\r\n';
+      }
+    }
+    list[i] = str;
+  }
+  return list.join('');
 }
 
 /**
  * 根据用户勾选的配置格式化文本
- * @param customProp 用户勾选的配置
  * @param text 原文本
- * @return string 格式化后文本
+ * @param customProp 用户勾选的配置
+ * @param set 文本内容设置函数（useState属性）
+ * @return TextPair 格式化后的文本对象
  */
-function handleText(customProp: CustomProperty, text: string) {
-  return { raw: text, formatted: handleChinese(customProp, text) };
-}
-
-function changeText(text: string, customProp: CustomProperty, set: (textPair: TextPair) => void) {
-  let handledTextObj = handleText(customProp, text);
+function handleText(text: string, customProp: CustomProperty, set: (textPair: TextPair) => void) {
+  let handledTextObj = { raw: text, formatted: handleChinese(customProp, text) };
   set(handledTextObj);
   if (customProp.rewriteClipboard) {
     // 写入粘贴板
     navigator.clipboard.writeText(handledTextObj.formatted).then();
   }
+  return handledTextObj;
 }
 
 /**
- * 处理剪切板内容：拷贝剪贴板内容到文本框，处理后，再通过 {@link changeText} 填充到粘贴板
+ * 处理成列表型内容
+ * @param text 原文本
+ * @param customProp 用户勾选的配置
+ * @param listProp 用户勾选的列表配置
+ * @param set 文本内容设置函数（useState 属性）
+ */
+function handleListText(
+  text: string,
+  customProp: CustomProperty,
+  listProp: ListProperty,
+  set: (textPair: TextPair) => void,
+) {
+  // 修改非列表下的参数
+  customProp.rewriteClipboard = false;
+  customProp.clearBreakLine = false;
+  // 先处理文本中的其他内容
+  let tempPair: TextPair = handleText(text, customProp, set);
+  tempPair.formatted = handleListChinese(listProp, tempPair.formatted);
+  set(tempPair);
+}
+
+/**
+ * 处理剪切板内容：拷贝剪贴板内容到文本框，处理后，再通过 {@link handleText} 填充到粘贴板
  * @param customProp 用户勾选的配置
  * @param set 设置文本内容函数
  */
@@ -106,7 +201,7 @@ async function handleClipboard(customProp: CustomProperty, set: (textPair: TextP
     return;
   }
   const clipboardText = await navigator.clipboard.readText();
-  changeText(clipboardText, customProp, set);
+  handleText(clipboardText, customProp, set);
 }
 
 // 初始化内容
@@ -120,15 +215,31 @@ const initialProp: CustomProperty = {
   rewriteClipboard: true,
   isHandleClipboard: true,
 };
+const initialListProp: ListProperty = {
+  removePrefixLength: 0,
+  endWith: '；',
+  lastWithPeriod: true,
+  serialMark: '1',
+  withBlankLine: true,
+};
 
 export default function TextFormatter() {
+  // 文本处理
   const [textObj, setTextObj] = useState(initialText);
   const [customProperty, setCustomProperty] = useState(initialProp);
   const [initialFlag, setInitialFlag] = useState(true);
+  // 列表内容处理
+  const [listProperty, setListProperty] = useState(initialListProp);
 
   function change(tempSingle: object) {
     setCustomProperty({ ...customProperty, ...tempSingle });
-    changeText(textObj.raw, { ...customProperty, ...tempSingle }, setTextObj);
+    handleText(textObj.raw, { ...customProperty, ...tempSingle }, setTextObj);
+  }
+
+  function listChange(tempSingle: object) {
+    let newProp = { ...listProperty, ...tempSingle };
+    setListProperty(newProp);
+    handleListText(textObj.raw, customProperty, newProp, setTextObj);
   }
 
   // 激活窗口
@@ -202,12 +313,33 @@ export default function TextFormatter() {
           复制到粘贴板
         </Checkbox>
       </div>
+      <div>
+        <Input
+          value={listProperty.removePrefixLength}
+          placeholder="前缀删除长度"
+          onChange={(e: React.SyntheticEvent): void => {
+            let content = e.target.value;
+            if (/^\d*$/.test(content)) {
+              // 只在输入为整数的情况下才进行处理
+              listChange({ removePrefixLength: content });
+            }
+          }}
+        />
+        <Checkbox
+          defaultChecked
+          onChange={(e: React.CheckboxChangeEvent): void => {
+            listChange({ rewriteClipboard: e.target.checked });
+          }}
+        >
+          添加空行
+        </Checkbox>
+      </div>
       <Input.TextArea
         showCount
         className={styles.textArea}
         value={textObj.raw}
         onChange={(e: React.SyntheticEvent): void => {
-          changeText(e.target.value, customProperty, setTextObj);
+          handleText(e.target.value, customProperty, setTextObj);
         }}
       />
       <Input.TextArea showCount className={styles.textArea} value={textObj.formatted} />
