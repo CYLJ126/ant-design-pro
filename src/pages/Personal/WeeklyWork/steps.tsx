592acb4a64143fbe79672cee6b6345c4329b11ba
zhangsc <zhangsc@fingard.com>
2025-02-25 17:17:09 +0800

    feat: 项目NobodyIsPerfect-002-日课-步骤支持设置完成，待办步骤和完成步骤显示不同的颜色

--- src/pages/Personal/WeeklyWork/steps.tsx
+++ src/pages/Personal/WeeklyWork/steps.tsx
@@ -10,17 +10,42 @@ import {
   MinusOutlined,
   PlusSquareOutlined,
   CheckOutlined,
+  UndoOutlined,
   SolutionOutlined,
 } from '@ant-design/icons';
 import styles from './steps.less';
 import { getSteps, saveSteps } from '@/services/ant-design-pro/dailyWork';
+import { createStyles } from 'antd-style';
 
-function Step({ index, initialContent, saveCurrentSteps }) {
+/**
+ * 根据传入颜色，设置每条步骤的颜色
+ * @param color 颜色
+ */
+const useStepStyle = (color) => {
+  return createStyles(({ css }) => ({
+    step: css`
+      .ant-input-group-addon {
+        border-color: ${color};
+        background-color: ${color};
+      }
+
+      .ant-input {
+        color: ${color};
+        border-color: ${color};
+      }
+    `,
+  }))();
+};
+
+function Step({ step, saveCurrentSteps }) {
+  const { orderId: index, status, content: initialContent } = { ...step };
   const [content, setContent] = useState(initialContent);
+  const { styles } = useStepStyle(status === 'INITIAL' ? '#81d3f8' : '#c6c6c6');
   return (
     <Input
       size="small"
       addonBefore={'Step' + index}
+      className={styles.step}
       addonAfter={
         <div>
           <PlusSquareOutlined
@@ -31,10 +56,18 @@ function Step({ index, initialContent, saveCurrentSteps }) {
             onClick={() => saveCurrentSteps(index, 'del', '')}
             style={{ marginRight: '3px' }}
           />
-          <CheckOutlined
-            onClick={() => saveCurrentSteps(index, 'done', '')}
-            style={{ marginRight: '3px' }}
-          />
+          {status === 'INITIAL' ? (
+            <CheckOutlined
+              onClick={() => saveCurrentSteps(index, 'done', '')}
+              style={{ marginRight: '3px' }}
+            />
+          ) : (
+            <UndoOutlined
+              onClick={() => saveCurrentSteps(index, 'todo', '')}
+              style={{ marginRight: '3px' }}
+            />
+          )}
+
           <SolutionOutlined />
         </div>
       }
@@ -72,7 +105,7 @@ export default function Steps({ targetId, deleteTarget }) {
    * 保存当前步骤列表
    * 如果是在删除步骤，且当前只有一个步骤，则删除当前事项
    * @param index 指定步骤下标
-   * @param type 操作类型，save-保存；del-删除当前步骤；add-在当前步骤后面添加新步骤；done-标记某个步骤为完成；
+   * @param type 操作类型，save-保存；del-删除当前步骤；add-在当前步骤后面添加新步骤；done-标记某个步骤为完成及todo-标记某个步骤为待办；
    * @param content 当前步骤调整内容
    */
   function saveCurrentSteps(index: number, type, content) {
@@ -91,7 +124,9 @@ export default function Steps({ targetId, deleteTarget }) {
         if (type === 'save') {
           steps[i - 1].content = content;
         } else if (type === 'done') {
-          steps[i - 1].status = 1;
+          steps[i - 1].status = 'DONE';
+        } else if (type === 'todo') {
+          steps[i - 1].status = 'INITIAL';
         } else if (type === 'del') {
           // 如果当前操作是删除，且下标等于指定下标，则删除当前步骤，即不添加到新的容器中
           continue;
@@ -102,7 +137,7 @@ export default function Steps({ targetId, deleteTarget }) {
         orderId: newIndex + 1,
         uuid: time + newIndex + 1,
         targetId: targetId,
-        status: steps[i - 1].status ?? 0,
+        status: steps[i - 1].status ?? 'INITIAL',
         summaryId: steps[i - 1].summaryId,
         progress: steps[i - 1].progress,
         content: steps[i - 1].content,
@@ -114,7 +149,7 @@ export default function Steps({ targetId, deleteTarget }) {
           orderId: newIndex + 1,
           uuid: time + newIndex + 1,
           targetId: targetId,
-          status: 0,
+          status: 'INITIAL',
           content: '',
           progress: 0,
         });
@@ -136,11 +171,7 @@ export default function Steps({ targetId, deleteTarget }) {
             .sort((a, b) => a.id - b.id)
             .map((item) => (
               <li key={item.uuid ?? item.orderId} className={styles.itemStep}>
-                <Step
-                  index={item.orderId}
-                  initialContent={item.content}
-                  saveCurrentSteps={saveCurrentSteps}
-                />
+                <Step step={item} saveCurrentSteps={saveCurrentSteps} />
               </li>
             ))}
         </ul>
