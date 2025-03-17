b2811e2de002d57435e0c19e9a822bddedcbcd59
zhangsc <zhangsc@fingard.com>
2025-03-17 08:47:52 +0800

    feat: 项目NobodyIsPerfect-002-日课-删除欢迎页，修改成首页内容

--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -1,8 +1,6 @@
 export default {
-  'menu.welcome': 'Welcome',
+  'menu.HomePage': 'Home Page',
   'menu.more-blocks': 'More Blocks',
-  'menu.home': 'Home',
-  'menu.admin': 'Admin',
   'menu.admin.sub-page': 'Sub-Page',
   'menu.login': 'Login',
   'menu.register': 'Register',
