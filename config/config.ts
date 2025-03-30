eb4fee6a311973f6921a372d771c8a410334b338
CYLJ126 <1586990974@qq.com>
2025-03-30 22:21:05 +0800

    feat: 项目NobodyIsPerfect-002-日课-调整项显示的目名

--- config/config.ts
+++ config/config.ts
@@ -86,7 +86,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: 'Nobody Is Perfect',
   layout: {
     locale: true,
     ...defaultSettings,
