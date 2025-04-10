c5fbafbfd969606c50475bc4b24cc0455babb8f9
CYLJ126 <1586990974@qq.com>
2025-04-10 23:25:14 +0800

    feat: 项目NobodyIsPerfect-003-首页-网站资讯-从后端获取新闻列表

--- src/pages/HomePage/WebsiteInfos.tsx
+++ src/pages/HomePage/WebsiteInfos.tsx
@@ -1,18 +1,21 @@
 import { Card, Carousel, Image, Popover, Space } from 'antd';
 import CSDN from '@/assets/learn/CSDN.png';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './WebsiteInfos.less';
+import { listWebsiteNews } from '@/services/ant-design-pro/homePage';
 
 const openWebsite = (url) => {
   // 打开新标签页
   window.open(url, '_blank', 'noopener, noreferrer');
 };
 
-function NewsCard({ news }) {
+function NewsCard({ news, width }) {
   return (
     <Card
       title={news.title}
+      hoverable
       className={styles.card}
+      style={{ width: width + 'px' }}
       size={'small'}
       onClick={() => openWebsite(news.url)}
     >
@@ -23,18 +26,21 @@ function NewsCard({ news }) {
 
 /**
  * 气泡卡片，悬浮显示新闻列表
+ *
  * @param newsList
  * @constructor
  */
 function PopoverList({ newsList }) {
   const time = new Date().getTime();
   return (
-    <Space direction={'vertical'} size={'small'} wrap={true}>
-      {newsList.map((news) => (
-        <>
-          <NewsCard news={news} key={news.title + '_' + time} />
-        </>
-      ))}
+    <Space direction={'vertical'} size={'small'} wrap={true} className={styles.popoverContent}>
+      <div>
+        {newsList.map((news) => (
+          <div style={{ marginBottom: '10px' }} key={news.title + '_' + time}>
+            <NewsCard news={news} width={800} />
+          </div>
+        ))}
+      </div>
     </Space>
   );
 }
@@ -43,13 +49,13 @@ function WebsiteInfo({ websiteParam }) {
   const { name, newsList } = websiteParam;
   const time = new Date().getTime();
   return (
-    <Popover content={<PopoverList newsList={newsList} />}>
+    <Popover autoAdjustOverflow content={<PopoverList newsList={newsList} />}>
       <Image width={100} preview={false} alt="CSDN" src={CSDN} />
       <span>{name}</span>
       {/* 走马灯 */}
-      <Carousel arrows autoplay className={styles.carousel}>
+      <Carousel arrows autoplay dotPosition={'top'} className={styles.carousel}>
         {newsList.map((news) => (
-          <NewsCard news={news} key={news.title + '_' + time} />
+          <NewsCard news={news} width={330} key={news.title + '_' + time} />
         ))}
       </Carousel>
     </Popover>
@@ -57,78 +63,11 @@ function WebsiteInfo({ websiteParam }) {
 }
 
 export default function WebsiteInfos() {
-  const content = [
-    {
-      name: '资讯',
-      newsList: [
-        {
-          title: 'C++变量深入理解',
-          summary: '极客头条」—— 技术人员的新闻圈',
-          url: 'https://blog.csdn.net/qq_21438461/article/details/146876417?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '完全指南XPath语法',
-          summary: '小米汽车支持Siri语音控车；苹果市值4天蒸发超5万亿元',
-          url: 'https://blog.csdn.net/weixin_66401877/article/details/146904839?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '使用URL API实现高效的URL解析',
-          summary: '腾讯张军回应微信“已读”功能；OpenAI对马斯克提起反诉',
-          url: 'https://blog.csdn.net/weixin_47754149/article/details/146371322?spm=1000.2115.3001.10524',
-        },
-      ],
-    },
-    {
-      name: '头条',
-      newsList: [
-        {
-          title: 'C++变量深入理解',
-          summary: 'Go占比首次突破3%，Kotlin和Swift退居',
-          url: 'https://blog.csdn.net/qq_21438461/article/details/146876417?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '完全指南XPath语法',
-          summary: '4 月 10 日 19:30，CSDN《万有引力》栏目特别邀请腾讯云开发者 AI 产品负责人汪',
-          url: 'https://blog.csdn.net/weixin_66401877/article/details/146904839?spm=1000.2115.3001.10524',
-        },
-      ],
-    },
-    {
-      name: '资讯',
-      newsList: [
-        {
-          title: 'C++变量深入理解',
-          summary: '极客头条」—— 技术人员的新闻圈',
-          url: 'https://blog.csdn.net/qq_21438461/article/details/146876417?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '完全指南XPath语法',
-          summary: '小米汽车支持Siri语音控车；苹果市值4天蒸发超5万亿元',
-          url: 'https://blog.csdn.net/weixin_66401877/article/details/146904839?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '使用URL API实现高效的URL解析',
-          summary: '腾讯张军回应微信“已读”功能；OpenAI对马斯克提起反诉',
-          url: 'https://blog.csdn.net/weixin_47754149/article/details/146371322?spm=1000.2115.3001.10524',
-        },
-      ],
-    },
-    {
-      name: '头条',
-      newsList: [
-        {
-          title: 'C++变量深入理解',
-          summary: 'Go占比首次突破3%，Kotlin和Swift退居',
-          url: 'https://blog.csdn.net/qq_21438461/article/details/146876417?spm=1000.2115.3001.10524',
-        },
-        {
-          title: '完全指南XPath语法',
-          summary: '4 月 10 日 19:30，CSDN《万有引力》栏目特别邀请腾讯云开发者 AI 产品负责人汪',
-          url: 'https://blog.csdn.net/weixin_66401877/article/details/146904839?spm=1000.2115.3001.10524',
-        },
-      ],
-    },
-  ];
+  const [websiteList, setWebsiteList] = useState([]);
+
+  useEffect(() => {
+    listWebsiteNews({}).then((result) => setWebsiteList(result));
+  }, []);
 
   const time = new Date().getTime();
   return (
@@ -140,7 +79,7 @@ export default function WebsiteInfos() {
         alignItems: 'stretch', // 元素高度对齐方式
       }}
     >
-      {content.map((item) => (
+      {websiteList.map((item) => (
         <WebsiteInfo key={item.name + '_' + time} websiteParam={item} />
       ))}
     </div>
