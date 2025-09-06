819069d414eb7b7ab5eb6f4b602e8f0073a0498f
CYLJ126 <1586990974@qq.com>
2025-09-06 23:09:33 +0800

    feat: 项目NobodyIsPerfect-002-留痕-展示样式改为默认收起

--- src/models/timeTraceFoldFlagModel.ts
+++ src/models/timeTraceFoldFlagModel.ts
@@ -4,7 +4,7 @@ import { useState } from 'react';
  * 用于传递时刻留痕数据折叠标记
  */
 export default () => {
-  const [foldFlag, setFoldFlag] = useState(false);
+  const [foldFlag, setFoldFlag] = useState(true);
 
   return {
     foldFlag,
