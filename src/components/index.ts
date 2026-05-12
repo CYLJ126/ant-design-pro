52806841ec03e3060b8d958c133c7fb8b3251ba4
zhangsc <zhangsc@fingard.com>
2026-05-12 17:58:53 +0800

    feat: 项目NIP-Front-000-升级-添加组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,48 +1,38 @@
 /**
- * 这个文件作为组件的目录
- * 目的是统一管理对外输出的组件，方便分类
- */
-/**
  * 布局组件
  */
 
-import DynamicForm from './DynamicForm';
 import { FormFieldConfig } from './DynamicForm/FormField';
 import Footer from './Footer';
-import MyColorPicker from './MyColorPicker';
 import { DocLink, LangDropdown, VersionDropdown } from './RightContent';
 import { AvatarDropdown } from './RightContent/AvatarDropdown';
-import SearchForm from './SearchForm';
-import type { ActionButton, TableColumn } from './SimpleTable';
-import SimpleTable from './SimpleTable';
-import TimeHeader from './TimeHeader';
-
-// import TagsSelector from './TagsSelector';
-// TagsSelector 添加后页面变成空白
-// export { default as TagsSelector } from './TagsSelector';
-
 /**
  * 业务组件
  */
+import type { ActionButton, TableColumn } from './SimpleTable';
+
 export { default as ArticleListContent } from './ArticleListContent';
 export { default as AvatarList } from './AvatarList';
+export { default as DynamicForm } from './DynamicForm';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as MyColorPicker } from './MyColorPicker';
 export { default as OfflineBanner } from './OfflineBanner';
+export { default as SearchForm } from './SearchForm';
+export { default as SimpleTable } from './SimpleTable';
 export { default as StandardFormRow } from './StandardFormRow';
 export { default as TagSelect } from './TagSelect';
+export { default as TimeHeader } from './TimeHeader';
+
+// TagsSelector 添加后页面变成空白
+// export { default as TagsSelector } from './TagsSelector';
 
 export {
   type ActionButton,
   AvatarDropdown,
   DocLink,
-  DynamicForm,
   Footer,
   FormFieldConfig,
   LangDropdown,
-  MyColorPicker,
-  SearchForm,
-  SimpleTable,
   type TableColumn,
-  TimeHeader,
   VersionDropdown,
 };
